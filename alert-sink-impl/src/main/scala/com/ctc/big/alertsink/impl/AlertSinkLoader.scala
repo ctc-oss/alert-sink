1089e3cee47d518668361f1368868807e40ca1ba
John Wass <wassj@ctc.com>
2017-06-07 07:59:19 -0400

    use static service locator for elasticsearch

--- alert-sink-impl/src/main/scala/com/ctc/big/alertsink/impl/AlertSinkLoader.scala
+++ alert-sink-impl/src/main/scala/com/ctc/big/alertsink/impl/AlertSinkLoader.scala
@@ -1,10 +1,12 @@
 package com.ctc.big.alertsink.impl
 
+import java.net.URI
+
 import akka.stream.ActorMaterializer
 import com.ctc.big.alertsink.api.AlertSinkService
 import com.lightbend.lagom.scaladsl.api.ServiceLocator
-import com.lightbend.lagom.scaladsl.api.ServiceLocator.NoServiceLocator
 import com.lightbend.lagom.scaladsl.broker.kafka.LagomKafkaComponents
+import com.lightbend.lagom.scaladsl.client.{CircuitBreakerComponents, StaticServiceLocator}
 import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
 import com.lightbend.lagom.scaladsl.persistence.jdbc.JdbcPersistenceComponents
 import com.lightbend.lagom.scaladsl.server._
@@ -19,12 +21,17 @@ class AlertSinkLoader extends LagomApplicationLoader {
 
   override def load(context: LagomApplicationContext): LagomApplication =
     new AlertSinkApplication(context) {
+      val esuri = elasticsearchUri
+      logger.warn(s"elasticsearch static service locator uri: $esuri")
 
-      override def serviceLocator: ServiceLocator = NoServiceLocator
+      override def serviceLocator: ServiceLocator = new StaticServiceLocator(URI.create(esuri), circuitBreakers)
     }
 
   override def loadDevMode(context: LagomApplicationContext): LagomApplication =
-    new AlertSinkApplication(context) with LagomDevModeComponents
+    new AlertSinkApplication(context) with LagomDevModeComponents {
+      val esuri = elasticsearchUri
+      logger.warn(s"elasticsearch static service locator uri: $esuri")
+    }
 
   override def describeServices = List(
     readDescriptor[AlertSinkService]
@@ -37,6 +44,8 @@ trait AlertSinkComponents extends LagomServerComponents with JdbcPersistenceComp
   override lazy val lagomServer = serverFor[AlertSinkService](wire[AlertSinkServiceImpl])
   override lazy val jsonSerializerRegistry = AlertSinkSerializerRegistry
 
+  def elasticsearchUri = configuration.getString("elasticsearch.uri").getOrElse("172.17.0.1:9200")
+
   persistentEntityRegistry.register(wire[AlertSinkEntity])
 }
 
@@ -45,6 +54,7 @@ abstract class AlertSinkApplication(context: LagomApplicationContext)
   extends LagomApplication(context)
           with AlertSinkComponents
           with LagomKafkaComponents
+          with CircuitBreakerComponents
           with AhcWSComponents {
 
   implicit val system = actorSystem
